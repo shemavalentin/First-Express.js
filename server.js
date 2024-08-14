d24ff81aafd67fd39d6c3224f62ec15bc99139ea
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-14 11:58:09 +0200

    Used Parameterized End Point to check one single person

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 // It's good to create express server in index.js file
 
 // The first thing is to import the express server
@@ -41,6 +42,91 @@ app.post("/messages", (req, res) => {
 });
 
 // Calling the listen function as in http request and set it on app to start listening
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
+
+// ============================================================================== */
+
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Let's have a list of freinds that we can add on or retrieve some
+const friends = [
+  {
+    id: 0,
+    name: "Albert Einstein",
+  },
+
+  {
+    id: 1,
+    name: "Sir Isaac Newtoon",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  // res.send(friends); //Here, express will set the contents type to application JSON
+  // because friends is an array of javascript objects as opposed to saying tring that
+  // will be slash HTML as the content type
+
+  // BUT EXPRESS HAS ANOTHER IMPORTANT FUNCTION TO DO THIS AS FOLLOW:
+  res.json(friends); // Now the data that we pass in will for sure be  treated as
+  // a JAVASCRIPT OBJECT NOTATION.
+});
+
+// HOW DO WE GET AN INDIVISUAL FRIEND? DO WE PASS THE URL THAT IS PASSED IN FROM REQ?
+// There is another way in express
+
+app.get("/friends/:friendId", (req, res) => {
+  // But the friend ID that we are getting from the params is actually a string
+  // because it's being grabbed straight from the URL. now befor we index our array
+  // we need to convert it to a number. we can add a (+) to do it, but let's use Nuumbet
+  // to be more explicit
+
+  const friendId = Number(req.params.friendId); // Notice how express populated the params property of request with the friendId
+
+  // Now let's be a little a bit responsible and validate our inputs as we don't know
+  // what the user put in after the slash to avoid unexpected bugs.
+  // It's always a good Idea to validate values that we not in control of like user inputs
+
+  const friend = friends[friendId]; // assigning a friend that is being selected
+
+  // if the Id that is passed in as a parameter(param), is not in the array
+  // this friend will be undefined. which means we need to check wether the friend is foind
+  if (friend) {
+    // if the friend is found? ....
+    // here also we can add the status to be more explicit even though express does
+    res.status(200).json(friend);
+  }
+
+  // When the user enters an ID which does not exist like /freinds/22 making a GET request
+  // Express will search for the different route handlers one by one, seeing if
+  // they match what was passed in the request. So, our parameterized route over here
+  // match the request which means that express won't return a 404 for this request
+  // because this it's matches one of our handlers.
+
+  // Now if we need to return a 404 because a friend wasn't found, we are going
+  // have to do it ourselves. we can set the status and send it manually in a few
+  // different ways. but let's do a good practice to send a meaningfull message
+  // to the client
+  else {
+    res.status(404).json({
+      // send an object of propertes
+      error: "Friend does not exist",
+    });
+  }
+});
+
+app.get("/messages", (req, res) => {
+  res.send("<ul><li> Hello Valentin</li></ul>");
+});
+
+app.post("/messages", (req, res) => {
+  console.log("Updating messages ...");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
