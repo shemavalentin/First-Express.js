5f8571db8ffd2f6a80183c83c04dd2b31e8cb2a5
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-16 11:49:15 +0200

    Sending Files with node, path and path.join

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,10 +1,22 @@
+const path = require("path"); // this will let us work with our paths handling all the different ways
+// that any different operating system uses paths on that kind of machine.
+
 // It's always a good idea to use named functions because when we are debugging our node
 // application  and we get one of those error messages, node can tell us the name of the
 // error in the log. Whereas when using arrow function it can't show the error even though
 // we assigned a function to a contant. This is function expression. node can't see that
 // function.
 function getMessages(req, res) {
-  res.send("<ul><li> Hello Valentin</li></ul>");
+  /* 
+In LINUX and MAC the file path looks like: folder/files.jpg
+In Windows we use: \files\files.jpg
+
+So to deal with these different scenarios, we're going to use the join function from the path module
+
+*/
+  // path.join(__dirname, "..", "public", "skimountain.jpeg"); // but this needs to be an absolute path. we have to use __dirname
+  // res.send("<ul><li> Hello Valentin</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpeg")); // To help express find our file, we're going to make use of the built-in path module.
 }
 
 function postMessage(req, res) {
--- routes/messages.router.js
+++ routes/messages.router.js
@@ -22,3 +22,18 @@ messagesRouter.post("/", messagesController.postMessage);
 module.exports = messagesRouter;
 
 // Go to the server.js and mount that router on the slash messages
+
+/* ***********************************************************
+SOMETIMES IT'S FINE TO SEND ROW DATA BACK TO THE USER. THAT'S WHAT WE'VE BEEN DOING SO FAR
+But sometimes our servers need to send entere files that already live on our server 
+machines back to the client.
+
+Express makes it very easy to do exactly that. Now for our message may our friend sent us a me
+messsage and it's not formatted as JSON or as HTML, but instead they uploaded a photo.
+We will put our files that we want to make availabe from our server in a new folder, which 
+we will call public folder.
+
+How do we now include this phoo when a user requests the messages collection from our server
+
+
+*/
